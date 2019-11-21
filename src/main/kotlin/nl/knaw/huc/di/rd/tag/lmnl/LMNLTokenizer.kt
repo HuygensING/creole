49145ea505033b8d423b31cccd822064eb1a577d
Bram Buitendijk <bram.buitendijk@huygens.knaw.nl>
2019-11-21 14:00:40 +0100

    [TRD-475] simplify lmnl tokenizer

--- src/main/kotlin/nl/knaw/huc/di/rd/tag/lmnl/LMNLTokenizer.kt
+++ src/main/kotlin/nl/knaw/huc/di/rd/tag/lmnl/LMNLTokenizer.kt
@@ -5,7 +5,6 @@ import lambdada.parsec.io.Reader
 import lambdada.parsec.parser.*
 import nl.knaw.huc.di.rd.tag.creole.Basics.qName
 import nl.knaw.huc.di.rd.tag.creole.Event
-import nl.knaw.huc.di.rd.tag.creole.events.EndTagEvent
 import nl.knaw.huc.di.rd.tag.creole.events.Events.endTagEvent
 import nl.knaw.huc.di.rd.tag.creole.events.Events.startTagEvent
 import nl.knaw.huc.di.rd.tag.creole.events.Events.textEvent
@@ -24,21 +23,21 @@ object LMNLTokenizer {
 
     val endTag = (char('{') thenRight tagName thenLeft char(']')).map { endTagEvent(qName(it)) }
 
-    val text = (not(tagDelimiter) /*or escapedTagDelimiter*/).optrep.map { textEvent(it.charsToString()) }
+    val text = (not(tagDelimiter) /*or escapedTagDelimiter*/).rep.map { textEvent(it.charsToString()) }
 
-    val lmnlParser = (startTag then (startTag or text or endTag).optrep then endTag then eos()).map { toEventList(it) }
+    val lmnlParser = (startTag then (startTag or text or endTag).optrep then eos()).map { toEventList(it) }
 //    val lmnlParser = (startTag then text then endTag then eos()).map { listOf(it.first.first.first, it.first.first.second, it.first.second) }
 
-    private fun toEventList(pair: Pair<Pair<Pair<StartTagEvent, List<Event>>, EndTagEvent>, Unit>): List<Event> {
-        return mutableListOf<Event>(pair.first.first.first).also {
-            it.addAll(pair.first.first.second)
-            it.add(pair.first.second)
+    private fun toEventList(pair: Pair<Pair<StartTagEvent, List<Event>>, Unit>): List<Event> {
+        return mutableListOf<Event>(pair.first.first).also {
+            it.addAll(pair.first.second)
         }
     }
 
     fun tokenize(lmnl: String): List<Event> {
         val lmnlReader = Reader.string(lmnl)
         val result = lmnlParser(lmnlReader)
+        println(result)
         return result.fold({ it.value }, { listOf() })
     }
 
